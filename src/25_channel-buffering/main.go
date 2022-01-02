0064ff84b81159b1e1bbc9ea2da15fa81c8c0816
blue32a <blue32a.jp@gmail.com>
2022-01-02 18:37:14 +0900

    Channel Buffering

---
+++ src/25_channel-buffering/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    messages := make(chan string, 2)
+    messages <- "bufferd"
+    messages <- "channel"
+    // messages <- "hoge" // error
+
+    fmt.Println(<-messages)
+    fmt.Println(<-messages)
+    // fmt.Println(<-messages)
+}
